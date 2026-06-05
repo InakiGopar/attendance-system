1a835f789acc62b518d8981dcd77826aa7a0235b
Ignacio Miguez <ignacio@bankuish.com>
2026-06-04 22:36:12 -0300

    feat: adding google oauth login, setting cors and adding .env variables

--- backend/src/main/java/com/backend/attendancesystem/auth/controller/AuthController.java
+++ backend/src/main/java/com/backend/attendancesystem/auth/controller/AuthController.java
@@ -2,12 +2,10 @@ package com.backend.attendancesystem.auth.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
